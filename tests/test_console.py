b6eed9f03e85dd7e61e679ea19dc2c19ed67f740
Amany Mohammed <amany.mohammd@gmail.com>
2024-05-22 05:26:19 +0300

    Add console tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,8 +1,111 @@
 #!/usr/bin/python3
 """ Console test module """
 import unittest
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
+from models import storage
 
 
-class TestConsole(unittest.TestCase):
-    """ Test Console """
-    pass
+class TestConsolePrompt(unittest.TestCase):
+    """test prompting of the command interpreter"""
+
+    def test_prompt_string(self):
+        """ test_prompt_string """
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    def test_empty_ine(self):
+        """ test_prompt_string"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", f.getvalue().strip())
+
+
+class TestConsoleExit(unittest.TestCase):
+    """Unittests for testing exiting from the HBNB command interpreter."""
+
+    def test_exit_exit(self):
+        """ test_exit_exit"""
+        with patch("sys.stdout", new=StringIO()) as output:
+            pass
+
+    def test_exit_EOF(self):
+        """ test_exit_EOF """
+        with patch("sys.stdout", new=StringIO()) as output:
+            pass
+
+
+class TestConsoleHelp(unittest.TestCase):
+    """test help command"""
+
+    def test_help_update(self):
+        """ test_help_update """
+        help = ("Updates an instance based on the class"
+                " name and id by adding or\n        "
+                "updating attribute (save the change into the JSON file).")
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help update"))
+            self.assertEqual(help, f.getvalue().strip())
+
+    def test_help(self):
+        """ test_help """
+        help = ("Documented commands (type help <topic>):\n"
+                "========================================\n"
+                "EOF  all  count  create  destroy  help  quit  show  update")
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help"))
+            self.assertEqual(help, f.getvalue().strip())
+
+    def test_cmd(self):
+        """ test_cmd """
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", f.getvalue().strip())
+
+    def test_command_object(self):
+        """ test_command_object """
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("create Review"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            testKey = "Review.{}".format(output.getvalue().strip())
+            self.assertIn(testKey, storage.all().keys())
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("create BaseModel"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            testKey = "BaseModel.{}".format(output.getvalue().strip())
+            self.assertIn(testKey, storage.all().keys())
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("create City"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            testKey = "City.{}".format(output.getvalue().strip())
+            self.assertIn(testKey, storage.all().keys())
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("create Amenity"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            testKey = "Amenity.{}".format(output.getvalue().strip())
+            self.assertIn(testKey, storage.all().keys())
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("create Place"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            testKey = "Place.{}".format(output.getvalue().strip())
+            self.assertIn(testKey, storage.all().keys())
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("create User"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            testKey = "User.{}".format(output.getvalue().strip())
+            self.assertIn(testKey, storage.all().keys())
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("create State"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            testKey = "State.{}".format(output.getvalue().strip())
+            self.assertIn(testKey, storage.all().keys())
+
+
+if __name__ == "__main__":
+    unittest.main()
